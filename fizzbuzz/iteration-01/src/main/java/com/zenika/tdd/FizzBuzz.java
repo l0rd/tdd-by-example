ffab7cb906b7fc0c83780ee35063deab5f6e767c
Mario Loriedo <mario.loriedo@gmail.com>
2015-07-27 15:14:05 +0200

    fizzbuzz-iteration-01

---
+++ fizzbuzz/iteration-01/src/main/java/com/zenika/tdd/FizzBuzz.java
@@ -0,0 +1,11 @@
+package com.zenika.tdd;
+
+/**
+ * Created by mariolet on 7/27/15.
+ */
+
+public class FizzBuzz {
+    public String printInteger(int i) {
+        return Integer.toString(i);
+    }
+}
---
+++ fizzbuzz/iteration-01/src/test/java/com/zenika/tdd/FizzBuzzTest.java
@@ -0,0 +1,18 @@
+package com.zenika.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mariolet on 7/27/15.
+ */
+public class FizzBuzzTest {
+
+    @Test
+    void printAnInteger() {
+        FizzBuzz fb = new FizzBuzz();
+        assertEquals("1", fb.printInteger(1));
+    }
+
+}
